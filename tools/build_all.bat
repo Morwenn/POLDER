a72efd241db65d2d28426f09af241bf5f813641b
Morwenn Edrahir <morwenn29@hotmail.fr>
2012-01-29 18:11:43 +0100

    Fixed the libraries generation.

--- tools/build_all.bat
+++ tools/build_all.bat
@@ -3,8 +3,14 @@ REM Under Windows using MinGW
 
 cd ..
 del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCOMPILE_MODE=Dynamic
 make
 del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCOMPILE_MODE=Static
+make
+del CMakeCache.txt
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCOMPILE_MODE=Dynamic
+make
+del CMakeCache.txt
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCOMPILE_MODE=Static
 make
--- tools/build_all.sh
+++ tools/build_all.sh
@@ -5,8 +5,14 @@
 
 cd ..
 rm -f CMakeCache.txt
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCOMPILE_MODE="Dynamic"
 make
 rm -f CMakeCache.txt
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCOMPILE_MODE="Static"
+make
+rm -f CMakeCache.txt
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCOMPILE_MODE="Dynamic"
+make
+rm -f CMakeCache.txt
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCOMPILE_MODE="Static"
 make
