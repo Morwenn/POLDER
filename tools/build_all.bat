18c96cb0b5424d051f3191a8cb77366bf43b52cd
Morwenn Edrahir <morwenn29@hotmail.fr>
2012-03-04 00:23:35 +0100

    Deleted useless instructions.

--- tools/build_all.bat
+++ tools/build_all.bat
@@ -6,23 +6,19 @@ REM Under Windows using MinGW
 cd ..
 
 echo Compiling the dynamic debug library...
-del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCOMPILE_MODE=Dynamic
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DCOMPILE_MODE="Dynamic"
 make
 
 echo Compiling the static debug library...
-del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCOMPILE_MODE=Static
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DCOMPILE_MODE="Static"
 make
 
 echo Compiling the dynamic release library...
-del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCOMPILE_MODE=Dynamic
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE="Release" -DCOMPILE_MODE="Dynamic"
 make
 
 echo Compiling the static release library...
-del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCOMPILE_MODE=Static
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE="Release" -DCOMPILE_MODE="Static"
 make
 
 echo Done
--- tools/build_all.sh
+++ tools/build_all.sh
@@ -4,15 +4,11 @@
 # Under Linux
 
 cd ..
-rm -f CMakeCache.txt
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCOMPILE_MODE="Dynamic"
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DCOMPILE_MODE="Dynamic"
 make
-rm -f CMakeCache.txt
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCOMPILE_MODE="Static"
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DCOMPILE_MODE="Static"
 make
-rm -f CMakeCache.txt
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCOMPILE_MODE="Dynamic"
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="Release" -DCOMPILE_MODE="Dynamic"
 make
-rm -f CMakeCache.txt
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCOMPILE_MODE="Static"
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="Release" -DCOMPILE_MODE="Static"
 make
