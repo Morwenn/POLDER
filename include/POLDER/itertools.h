1203926f7202bf54f9c96acb7c33a3f39835b840
Morwenn Edrahir <morwenn29@hotmail.fr>
2012-06-14 21:25:40 +0200

    New Python-inspired generator API.

---
+++ include/POLDER/itertools.h
@@ -0,0 +1,179 @@
+/*
+ * Copyright (C) 2011-2012 Morwenn
+ *
+ * POLDER is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * POLDER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _POLDER_ITERTOOLS_H
+#define _POLDER_ITERTOOLS_H
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <POLDER/config.h>
+
+
+namespace polder
+{
+
+/**
+ * @namespace polder::itertools
+ * @brief Iterator tools
+ *
+ * Collection of functions an functor inspired
+ * from Python programming. Many of them are meant
+ * be used with the C++11 for loop.
+ */
+namespace itertools
+{
+
+
+/**
+ * @brief Range of integers
+ */
+class POLDER_API range
+{
+    public:
+
+        constexpr range(int end):
+            _i(0),
+            _end(end),
+            _step(1),
+            _sup(end >= 0)
+        {}
+
+        constexpr range(int begin, int end, unsigned int step=1):
+            _i(begin),
+            _end(end),
+            _step(step),
+            _sup(end >= begin)
+        {}
+
+        constexpr const range& begin()
+        {
+            return *this;
+        }
+
+        constexpr const range& end() const
+        {
+            return *this;
+        }
+
+        constexpr bool operator!=(const range&) const
+        {
+            return _sup ? (_i < _end) : (_i > _end);
+        }
+
+        inline void operator++()
+        {
+            if (_sup)
+            {
+                _i += _step;
+            }
+            else
+            {
+                _i -= _step;
+            }
+        }
+
+        constexpr const int& operator*() const
+        {
+            return _i;
+        }
+
+    private:
+
+        int _i;
+        const int _end;
+        const unsigned int _step;
+        const bool _sup;
+};
+
+
+/**
+ * @brief Reversed iterable
+ */
+template<typename ReverseIterable>
+class POLDER_API __reversed
+{
+    // FUTURE: Could be improved with concepts
+
+    private:
+
+        ReverseIterable& _iter;
+
+    public:
+
+        __reversed(ReverseIterable& iter):
+            _iter(iter)
+        {}
+
+        auto begin() -> decltype(_iter.rbegin())
+        {
+            return _iter.rbegin();
+        }
+
+        auto end() -> decltype(_iter.rend())
+        {
+            return _iter.rend();
+        }
+};
+
+template<typename ReverseIterable>
+POLDER_API inline __reversed<ReverseIterable> reversed(ReverseIterable& iter)
+{
+    return __reversed<ReverseIterable>(iter);
+}
+
+/**
+ * @brief Constant reversed iterable
+ */
+template<typename ReverseIterable>
+class POLDER_API __creversed
+{
+    // FUTURE: Could be improved with concepts
+
+    private:
+
+        const ReverseIterable& _iter;
+
+    public:
+
+        __creversed(const ReverseIterable& iter):
+            _iter(iter)
+        {}
+
+        auto begin() -> decltype(_iter.crbegin())
+        {
+            return _iter.crbegin();
+        }
+
+        auto end() -> decltype(_iter.crend())
+        {
+            return _iter.crend();
+        }
+};
+
+template<typename ReverseIterable>
+POLDER_API inline const __reversed<ReverseIterable> reversed(const ReverseIterable& iter)
+{
+    return __reversed<ReverseIterable>(iter);
+}
+
+
+} // namespace itertools
+} // namespace polder
+
+
+#endif // _POLDER_ITERTOOLS_H
+
