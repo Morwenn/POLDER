5b89ca7ea681dfcc61414cb4e94a4f243fcc2b38
Morwenn Edrahir <morwenn29@hotmail.fr>
2012-06-23 13:26:43 +0200

    Added an example for fgetl.

--- examples/io.cpp
+++ examples/io.cpp
@@ -1,21 +1,62 @@
 // Headers
+#include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <string>
 #include <POLDER/io.h>
 
-
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::string;
+using polder::io::fgetl;
 using polder::io::print;
 
+
+/**
+ * @brief Entry point of application
+ *
+ * @return Application exit code
+ */
 int main()
 {
-    int a = 5;
-    double b = 6.3;
-    std::string c = "Hello";
+    ////////////////////////////////////////////////////////////
+    {
+        // For people who like C
+        cout << "fgetl example" << endl;
 
-    // This function is some kind of wrapper around std::ostream.
-    // You can pass any type that can be added to an ostream by
-    // the << operator.
-    print(a, b, c, "World!");
+        // Open a file
+        FILE* f = fopen("io_example.txt", "r");
+        if (f == nullptr)
+        {
+            cerr << "Can't open the file.'" << endl;
+            exit(1);
+        }
+
+        // line is the file reader
+        char* line = nullptr;
+        // This function reads the file the same way fgets does
+        // However, it can read a line, no matter its size.
+        while (fgetl(line, f))
+        {
+            cout << line;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////
+    {
+        // For people who like Python
+        cout << endl << "print example" << endl;
+
+        int a = 5;
+        double b = 6.3;
+        string c = "Hello";
+
+        // This function is some kind of wrapper around std::ostream.
+        // You can pass any type that can be added to an ostream by
+        // the << operator.
+        print(a, b, c, "World!");
+    }
 
     return EXIT_SUCCESS;
 }
