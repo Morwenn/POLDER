d419e42b4e9de4e32fdb0d3c16f1eea4bf71a46d
Morwenn Edrahir <morwenn29@hotmail.fr>
2012-07-23 21:06:21 +0200

    Cleaned a little.

--- examples/path.cpp
+++ examples/path.cpp
@@ -6,13 +6,16 @@
 #include <string>
 #include <POLDER/path.h>
 
+using std::cout;
+using std::endl;
+using std::string;
 using polder::path::make_path;
 
 
 int main()
 {
-    std::string path = make_path("trololo", "lala", "lulu");
-    std::cout << path << std::endl;
+    string path = make_path("trololo", "lala", "lulu");
+    cout << path << endl;
 
     return EXIT_SUCCESS;
 }
