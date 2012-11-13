c62d99a396fa80789dcf28d79eeda162b0a37d41
Morwenn Edrahir <morwenn29@hotmail.fr>
2012-11-13 21:18:49 +0100

    Added variadic is_integral and is_floating_point structs.

--- include/POLDER/type_traits.h
+++ include/POLDER/type_traits.h
@@ -20,7 +20,7 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#include <cinttypes>
+#include <cstdint>
 #include <type_traits>
 #include <POLDER/config.h>
 
@@ -42,6 +42,48 @@ namespace polder
         U
     >;
 
+    template<typename...>
+    struct is_integral;
+
+    template<typename T>
+    struct is_integral<T>:
+        public std::conditional<
+            std::is_integral<T>::value,
+            std::true_type,
+            std::false_type
+        >::type
+    {};
+
+    template<typename T, typename U, typename... Args>
+    struct is_integral<T, U, Args...>:
+        public std::conditional<
+            std::is_integral<T>::value,
+            is_integral<U, Args...>,
+            std::false_type
+        >::type
+    {};
+
+    template<typename...>
+    struct is_floating_point;
+
+    template<typename T>
+    struct is_floating_point<T>:
+        public std::conditional<
+            std::is_floating_point<T>::value,
+            std::true_type,
+            std::false_type
+        >::type
+    {};
+
+    template<typename T, typename U, typename... Args>
+    struct is_floating_point<T, U, Args...>:
+        public std::conditional<
+            std::is_floating_point<T>::value,
+            is_floating_point<U, Args...>,
+            std::false_type
+        >::type
+    {};
+
     /**
      * @brief Unisgned type with at least the given number of bits
      *
@@ -58,42 +100,42 @@ namespace polder
         using type = unsigned long long int;
     };
 
-    template<> struct uint_least<0> { using type = uint_least8_t; };
-    template<> struct uint_least<1> { using type = uint_least8_t; };
-    template<> struct uint_least<2> { using type = uint_least8_t; };
-    template<> struct uint_least<3> { using type = uint_least8_t; };
-    template<> struct uint_least<4> { using type = uint_least8_t; };
-    template<> struct uint_least<5> { using type = uint_least8_t; };
-    template<> struct uint_least<6> { using type = uint_least8_t; };
-    template<> struct uint_least<7> { using type = uint_least8_t; };
-    template<> struct uint_least<8> { using type = uint_least8_t; };
-    template<> struct uint_least<9> { using type = uint_least16_t; };
-    template<> struct uint_least<10> { using type = uint_least16_t; };
-    template<> struct uint_least<11> { using type = uint_least16_t; };
-    template<> struct uint_least<12> { using type = uint_least16_t; };
-    template<> struct uint_least<13> { using type = uint_least16_t; };
-    template<> struct uint_least<14> { using type = uint_least16_t; };
-    template<> struct uint_least<15> { using type = uint_least16_t; };
-    template<> struct uint_least<16> { using type = uint_least16_t; };
-    template<> struct uint_least<17> { using type = uint_least32_t; };
-    template<> struct uint_least<18> { using type = uint_least32_t; };
-    template<> struct uint_least<19> { using type = uint_least32_t; };
-    template<> struct uint_least<20> { using type = uint_least32_t; };
-    template<> struct uint_least<21> { using type = uint_least32_t; };
-    template<> struct uint_least<22> { using type = uint_least32_t; };
-    template<> struct uint_least<23> { using type = uint_least32_t; };
-    template<> struct uint_least<24> { using type = uint_least32_t; };
-    template<> struct uint_least<25> { using type = uint_least32_t; };
-    template<> struct uint_least<26> { using type = uint_least32_t; };
-    template<> struct uint_least<27> { using type = uint_least32_t; };
-    template<> struct uint_least<28> { using type = uint_least32_t; };
-    template<> struct uint_least<29> { using type = uint_least32_t; };
-    template<> struct uint_least<30> { using type = uint_least32_t; };
-    template<> struct uint_least<31> { using type = uint_least32_t; };
-    template<> struct uint_least<32> { using type = uint_least32_t; };
+    template<> struct uint_least<0>     { using type = uint_least8_t; };
+    template<> struct uint_least<1>     { using type = uint_least8_t; };
+    template<> struct uint_least<2>     { using type = uint_least8_t; };
+    template<> struct uint_least<3>     { using type = uint_least8_t; };
+    template<> struct uint_least<4>     { using type = uint_least8_t; };
+    template<> struct uint_least<5>     { using type = uint_least8_t; };
+    template<> struct uint_least<6>     { using type = uint_least8_t; };
+    template<> struct uint_least<7>     { using type = uint_least8_t; };
+    template<> struct uint_least<8>     { using type = uint_least8_t; };
+    template<> struct uint_least<9>     { using type = uint_least16_t; };
+    template<> struct uint_least<10>    { using type = uint_least16_t; };
+    template<> struct uint_least<11>    { using type = uint_least16_t; };
+    template<> struct uint_least<12>    { using type = uint_least16_t; };
+    template<> struct uint_least<13>    { using type = uint_least16_t; };
+    template<> struct uint_least<14>    { using type = uint_least16_t; };
+    template<> struct uint_least<15>    { using type = uint_least16_t; };
+    template<> struct uint_least<16>    { using type = uint_least16_t; };
+    template<> struct uint_least<17>    { using type = uint_least32_t; };
+    template<> struct uint_least<18>    { using type = uint_least32_t; };
+    template<> struct uint_least<19>    { using type = uint_least32_t; };
+    template<> struct uint_least<20>    { using type = uint_least32_t; };
+    template<> struct uint_least<21>    { using type = uint_least32_t; };
+    template<> struct uint_least<22>    { using type = uint_least32_t; };
+    template<> struct uint_least<23>    { using type = uint_least32_t; };
+    template<> struct uint_least<24>    { using type = uint_least32_t; };
+    template<> struct uint_least<25>    { using type = uint_least32_t; };
+    template<> struct uint_least<26>    { using type = uint_least32_t; };
+    template<> struct uint_least<27>    { using type = uint_least32_t; };
+    template<> struct uint_least<28>    { using type = uint_least32_t; };
+    template<> struct uint_least<29>    { using type = uint_least32_t; };
+    template<> struct uint_least<30>    { using type = uint_least32_t; };
+    template<> struct uint_least<31>    { using type = uint_least32_t; };
+    template<> struct uint_least<32>    { using type = uint_least32_t; };
 
 } // namespace polder
 
 
-#endif // _POLDER_EXCEPTIONS_H
+#endif // _POLDER_TYPE_TRAITS_H
 
