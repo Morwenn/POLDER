b6af1af0b601761764662facf707c4ed09f69cb0
Morwenn <morwenn29@hotmail.fr>
2013-01-31 15:56:09 +0100

    Removed useless experimental feature.

--- include/POLDER/type_traits.h
+++ include/POLDER/type_traits.h
@@ -171,56 +171,6 @@ namespace polder
         >::type
     {};
 
-    /**
-     * @brief Unisgned type with at least the given number of bits
-     *
-     * This type is an extension of the standard types
-     * uint_leastN_t to give an unsigned integer with at
-     * least the required number of bits.
-     */
-    template<uint8_t nb_bits>
-    struct uint_least
-    {
-        // Biggest standard unsigned integer type
-        // intmax_t and uint_least64_t are not supported everywhere
-        // hence the use of unsigned long long which is better supported
-        using type = unsigned long long int;
-    };
-
-    template<> struct uint_least<0>     { using type = uint_least8_t; };
-    template<> struct uint_least<1>     { using type = uint_least8_t; };
-    template<> struct uint_least<2>     { using type = uint_least8_t; };
-    template<> struct uint_least<3>     { using type = uint_least8_t; };
-    template<> struct uint_least<4>     { using type = uint_least8_t; };
-    template<> struct uint_least<5>     { using type = uint_least8_t; };
-    template<> struct uint_least<6>     { using type = uint_least8_t; };
-    template<> struct uint_least<7>     { using type = uint_least8_t; };
-    template<> struct uint_least<8>     { using type = uint_least8_t; };
-    template<> struct uint_least<9>     { using type = uint_least16_t; };
-    template<> struct uint_least<10>    { using type = uint_least16_t; };
-    template<> struct uint_least<11>    { using type = uint_least16_t; };
-    template<> struct uint_least<12>    { using type = uint_least16_t; };
-    template<> struct uint_least<13>    { using type = uint_least16_t; };
-    template<> struct uint_least<14>    { using type = uint_least16_t; };
-    template<> struct uint_least<15>    { using type = uint_least16_t; };
-    template<> struct uint_least<16>    { using type = uint_least16_t; };
-    template<> struct uint_least<17>    { using type = uint_least32_t; };
-    template<> struct uint_least<18>    { using type = uint_least32_t; };
-    template<> struct uint_least<19>    { using type = uint_least32_t; };
-    template<> struct uint_least<20>    { using type = uint_least32_t; };
-    template<> struct uint_least<21>    { using type = uint_least32_t; };
-    template<> struct uint_least<22>    { using type = uint_least32_t; };
-    template<> struct uint_least<23>    { using type = uint_least32_t; };
-    template<> struct uint_least<24>    { using type = uint_least32_t; };
-    template<> struct uint_least<25>    { using type = uint_least32_t; };
-    template<> struct uint_least<26>    { using type = uint_least32_t; };
-    template<> struct uint_least<27>    { using type = uint_least32_t; };
-    template<> struct uint_least<28>    { using type = uint_least32_t; };
-    template<> struct uint_least<29>    { using type = uint_least32_t; };
-    template<> struct uint_least<30>    { using type = uint_least32_t; };
-    template<> struct uint_least<31>    { using type = uint_least32_t; };
-    template<> struct uint_least<32>    { using type = uint_least32_t; };
-
 } // namespace polder
 
 
