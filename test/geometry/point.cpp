0bc34a558b625d9d7f580693ffcf70f007f290b1
Morwenn <morwenn29@hotmail.fr>
2014-05-07 16:10:40 +0200

    Added tests for Vector<N, T> ans Point<N, T>.

---
+++ test/geometry/point.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2014 Morwenn
+ *
+ * POLDER is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * POLDER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+#include <POLDER/geometry/point.h>
+#include <POLDER/geometry/vector.h>
+
+int main()
+{
+    using namespace polder;
+
+    // TEST: Point basic operations
+    // - construction
+    // - indexing
+    // relational operators
+    {
+        using Point = geometry::Point<2u, int>;
+
+        Point a(48, 23);
+        POLDER_ASSERT(a[0] == 48);
+        POLDER_ASSERT(a[1] == 23);
+        POLDER_ASSERT(a.x() == 48);
+        POLDER_ASSERT(a.y() == 23);
+        POLDER_ASSERT(*(a.begin()) == 48);
+
+        Point b = { 48, 23 };
+        POLDER_ASSERT(b[0] == 48);
+        POLDER_ASSERT(b[1] == 23);
+        POLDER_ASSERT(b.x() == 48);
+        POLDER_ASSERT(b.y() == 23);
+        POLDER_ASSERT(*(b.begin()) == 48);
+
+        POLDER_ASSERT(a == b);
+        POLDER_ASSERT(not (a != b));
+        POLDER_ASSERT(Point(2, 3) != a);
+
+        Point foo;
+        (void) foo;
+    }
+
+    // TEST: Point-Point subtraction
+    {
+        using Point = geometry::Point<2u, int>;
+        using Vector = geometry::Vector<2u, int>;
+
+        Point a = { 2, 3 };
+        Point b = { 6, 8 };
+
+        POLDER_ASSERT(b-a == Vector(4, 5));
+        POLDER_ASSERT(a-b == Vector(-4, -5));
+        POLDER_ASSERT(b-a == -(a-b));
+    }
+
+    // TEST: Point-Vector operations
+    // - addition
+    // - subtraction
+    {
+        using Point = geometry::Point<2u, int>;
+        using Vector = geometry::Vector<2u, int>;
+
+        Point pt = { 5, -3 };
+        Vector vec = { -1, 8 };
+
+        POLDER_ASSERT(pt+vec == Point(4, 5));
+        POLDER_ASSERT(pt-vec == Point(6, -11));
+        POLDER_ASSERT(pt+vec == vec+pt);
+    }
+}
---
+++ test/geometry/vector.cpp
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2014 Morwenn
+ *
+ * POLDER is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * POLDER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+#include <POLDER/algorithm.h>
+#include <POLDER/geometry/vector.h>
+
+int main()
+{
+    using namespace polder;
+
+    // TEST: Vector basic operations
+    // - construction
+    // - indexing
+    // relational operators
+    {
+        using Vector = geometry::Vector<2u, int>;
+
+        Vector a(48, 23);
+        POLDER_ASSERT(a[0] == 48);
+        POLDER_ASSERT(a[1] == 23);
+        POLDER_ASSERT(a.x() == 48);
+        POLDER_ASSERT(a.y() == 23);
+        POLDER_ASSERT(*(a.begin()) == 48);
+
+        Vector b = { 48, 23 };
+        POLDER_ASSERT(b[0] == 48);
+        POLDER_ASSERT(b[1] == 23);
+        POLDER_ASSERT(b.x() == 48);
+        POLDER_ASSERT(b.y() == 23);
+        POLDER_ASSERT(*(b.begin()) == 48);
+
+        POLDER_ASSERT(a == b);
+        POLDER_ASSERT(not (a != b));
+        POLDER_ASSERT(Vector(2, 3) != a);
+
+        Vector foo;
+        (void) foo;
+    }
+
+    // TEST: Vector-scalar multiplication
+    {
+        using Vector = geometry::Vector<2u, int>;
+
+        Vector a = { 3, 5 };
+
+        POLDER_ASSERT(2*a == Vector(6, 10));
+        POLDER_ASSERT(a*3 == Vector(9, 15));
+        POLDER_ASSERT(a*15 == 15*a);
+    }
+
+    // TEST: Vector-scalar division
+    {
+        using Vector = geometry::Vector<2u, int>;
+
+        Vector a = { 12, 6 };
+
+        POLDER_ASSERT(a/2 == Vector(6, 3));
+        POLDER_ASSERT(a/3 == Vector(4, 2));
+        POLDER_ASSERT(a/2*2 == a);
+        POLDER_ASSERT(a == 3*a/3);
+    }
+
+    // TEST: dot product
+    {
+        using Vector2 = geometry::Vector<2u, int>;
+        using Vector3 = geometry::Vector<3u, int>;
+
+        Vector3 a = { 1, 3, -5 };
+        Vector3 b = { 4, -2, -1 };
+        POLDER_ASSERT(a*b == 3);
+        POLDER_ASSERT(a*b == b*a);
+
+        Vector3 c = { 1, 2, 3 };
+        Vector3 d = { 4, -5, 6 };
+        POLDER_ASSERT(c*d == 12);
+
+        Vector2 e = { -4, -9 };
+        Vector2 f = { -1, 2 };
+        POLDER_ASSERT(e*f == -14);
+    }
+
+    // TEST: Vector arithmetic
+    // - addition
+    // - subtraction
+    // - opposite
+    {
+        using Vector = geometry::Vector<2u, int>;
+
+        Vector a = { 2, 3 };
+        Vector b = { 6, 8 };
+
+        POLDER_ASSERT(-a == Vector(-2, -3));
+        POLDER_ASSERT(-b == Vector(-6, -8));
+        POLDER_ASSERT(a+b == Vector(8, 11));
+        POLDER_ASSERT(a+b == b+a);
+        POLDER_ASSERT(b-a == Vector(4, 5));
+        POLDER_ASSERT(a-b == Vector(-4, -5));
+        POLDER_ASSERT(b-a == -(a-b));
+        POLDER_ASSERT(a-b == a+(-b));
+    }
+
+    // TEST: Vector norms
+    {
+        using polder::float_equal;
+        using namespace polder::geometry;
+        using namespace polder::math::norm;
+
+        Vector<3u, double> test = { 4.0, -2.0, 6.0 };
+
+        POLDER_ASSERT(test.norm() == test.norm<euclidean>());
+        POLDER_ASSERT(float_equal(
+            test.norm<manhattan>(), 12.0
+        ));
+        POLDER_ASSERT(float_equal(
+            test.norm<euclidean>(), 7.483314773547883
+        ));
+        POLDER_ASSERT(float_equal(
+            test.norm<maximum>(), 6.0
+        ));
+
+        POLDER_ASSERT(test.norm(3u) == test.norm<p>(3u));
+        POLDER_ASSERT(float_equal(
+            test.norm<p>(3u), 6.603854497789253
+        ));
+        POLDER_ASSERT(float_equal(
+            test.norm<p>(5u), 6.154769770788126
+        ));
+    }
+}
